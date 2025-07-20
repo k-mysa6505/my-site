26029a64f3f45c78aa7bab72b42cd8fd3b277f81
kokko <kota.mysa6505@gmail.com>
2025-07-20 23:54:10 +0900

    画像ファイルの整理

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,8 +1,8 @@
 export const SITE_NAME = "Kokko's Blog";
 export const SITE_DESCRIPTION = "MYSのホームページ";
 export const SNS_LINKS = [
-  { href: 'https://github.com/', icon: '/images/icons8-github.png', alt: 'GitHub' },
-  { href: 'https://twitter.com/', icon: '/images/icons8-twitter.png', alt: 'Twitter' },
-  { href: 'https://facebook.com', icon: '/images/icons8-facebook.png', alt: 'Facebook' },
-  { href: 'https://instagram.com/', icon: '/images/icons8-instagram.png', alt: 'Instagram' },
+  { href: 'https://github.com/', icon: '/images/icons/icons8-github.png', alt: 'GitHub' },
+  { href: 'https://twitter.com/', icon: '/images/icons/icons8-twitter.png', alt: 'Twitter' },
+  { href: 'https://facebook.com', icon: '/images/icons/icons8-facebook.png', alt: 'Facebook' },
+  { href: 'https://instagram.com/', icon: '/images/icons/icons8-instagram.png', alt: 'Instagram' },
 ]; 
